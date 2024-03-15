28d5da13f9a0324d22503816877ebe07887ec84d
Vadim <vadgab9@gmail.com>
2024-03-15 10:41:36 +0500

    add sort

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Basket from "./components/button";
 import Header from "./components/header";
 import data from './data.json';
 import Pizza from "./components/pizza";
+import Sort from "./components/sort";
 
 console.log(data)
 
@@ -13,12 +14,24 @@ function App() {
   const [collections,setCollections]=useState([]);
   const [category,setCattegory]=useState([]);
   const [changeTab,setChangeTab]=useState(0);
+  const [sort,setSort] = useState(false);
+  const [picksort,setPickSort]=useState(0);
   useEffect(() => {
     setCollections(data.items);
     setCattegory(data.category)
   }, []);
   function onChangeTab(index){
     setChangeTab(index)
+  }
+  function openModal(){
+    if (sort==true){
+      setSort(false)
+    }else{
+      setSort(true)
+    }
+  }
+  function changeSort(index){
+    setPickSort(index)
   }
   return (
       <div className="wrapper">
@@ -35,31 +48,7 @@ function App() {
                   }
                 </ul>
               </div>
-              <div className="sort">
-                <div className="sort__label">
-                  <svg
-                      width="10"
-                      height="6"
-                      viewBox="0 0 10 6"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                        d="M10 5C10 5.16927 9.93815 5.31576 9.81445 5.43945C9.69075 5.56315 9.54427 5.625 9.375 5.625H0.625C0.455729 5.625 0.309245 5.56315 0.185547 5.43945C0.061849 5.31576 0 5.16927 0 5C0 4.83073 0.061849 4.68424 0.185547 4.56055L4.56055 0.185547C4.68424 0.061849 4.83073 0 5 0C5.16927 0 5.31576 0.061849 5.43945 0.185547L9.81445 4.56055C9.93815 4.68424 10 4.83073 10 5Z"
-                        fill="#2C2C2C"
-                    />
-                  </svg>
-                  <b>Сортировка по:</b>
-                  <span>популярности</span>
-                </div>
-                <div className="sort__popup">
-                  <ul>
-                    <li className="active">популярности</li>
-                    <li>цене</li>
-                    <li>алфавиту</li>
-                  </ul>
-                </div>
-              </div>
+              <Sort changeSort={changeSort} sort={sort} openModal={openModal} picksort={picksort} />
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
@@ -69,6 +58,16 @@ function App() {
                     return true
                   }else {
                     return obj.category === changeTab;
+                  }
+                }).sort((a, b) => {
+                  if (picksort === 0) {
+                    return b.popular - a.popular; // Сортировка по популярности от большего к меньшему
+                  } else if (picksort === 1) {
+                    return a.baseprice - b.baseprice; // Сортировка по цене от меньшего к большему
+                  } else if (picksort === 2) {
+                    return a.name.localeCompare(b.name); // Сортировка по алфавиту от A до Z
+                  } else {
+                    return 0; // Без сортировки
                   }
                 }).map((obj,index)=>(
                     <Pizza
